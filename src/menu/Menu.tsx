1e3e8fe34aa9e20238bcdd5ad5be7052b4291e7b
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-02-05 07:46:23 +0100

    Почав виконання ДЗ

--- src/menu/Menu.tsx
+++ src/menu/Menu.tsx
@@ -1,21 +1,24 @@
 import Link from "next/link";
+import "./Menu.css";
 
 const Menu = () => {
     return (
-        <ul>
-            <li>
-                <Link href="/">Home</Link>
-            </li>
-            <li>
-                <Link href="/users">Users</Link>
-            </li>
-            <li>
-                <Link href="/posts">Posts</Link>
-            </li>
-            <li>
-                <Link href="/comments">Comments</Link>
-            </li>
-        </ul>
+        <nav className="menu-container">
+            <ul>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/users">Users</Link>
+                </li>
+                <li>
+                    <Link href="/posts">Posts</Link>
+                </li>
+                <li>
+                    <Link href="/comments">Comments</Link>
+                </li>
+            </ul>
+        </nav>
     );
 };
 export default Menu;
