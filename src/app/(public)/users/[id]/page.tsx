4422f9549f298ba416a8108f79f5a7fb099e0e9a
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-02-06 06:21:04 +0100

    почав працювати по другому відео з pass data between pages

--- src/app/(public)/users/[id]/page.tsx
+++ src/app/(public)/users/[id]/page.tsx
@@ -1,10 +1,11 @@
 import {FC} from "react";
 import {Metadata} from "next";
+import {SearchParams} from "next/dist/server/request/search-params";
 
 type Props = {
     params: { id: string };
+    searchParams: SearchParams;
 }
-
 
 export const generateMetadata = async ({params}:Props): Promise<Metadata> => {
     const {id} = await params;
@@ -12,7 +13,8 @@ export const generateMetadata = async ({params}:Props): Promise<Metadata> => {
         title: 'User page title ' + id,
     }
 }
-const UserPage: FC<Props> = async ({params}) => {
+
+const UserPage: FC<Props> = async ({params, searchParams}) => {
     const {id} = await params;
     return (
         <div>
