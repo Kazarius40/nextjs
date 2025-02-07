0d0734edcffa6282190ec9c60427387c378fc7ae
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-02-07 15:14:14 +0100

    Завершив виконання ДЗ

--- src/app/cars/page.tsx
+++ src/app/cars/page.tsx
@@ -1,14 +1,18 @@
 import CarsComponent from "@/CarsComponent/CarsComponent";
 import {getAllCars} from "@/services/carService";
 
-const CarsPage = async () => {
+
+const CarsPage =  async () => {
 
     const cars = await getAllCars();
 
+
     return (
-        <>
+        <div>
+
+
             <CarsComponent item={cars} />
-        </>
+        </div>
     );
 };
 
--- src/services/carService.ts
+++ src/services/carService.ts
@@ -1,7 +1,16 @@
-const baseUrl = 'http://owu.linkpc.net/carsAPI/v1';
+import axios from "axios";
+import {ICar} from "@/models/ICar";
 
+const axiosInstance = axios.create({
+    baseURL: 'http://185.69.152.209/carsAPI/v1',
+    headers: {'Content-Type': 'application/json'}
+})
 
-export const getAllCars = async () => {
-    return await fetch(`${baseUrl}/cars`)
-        .then(res => res.json());
+export const getAllCars = async (): Promise<ICar[]> => {
+    const {data} = await axiosInstance.get<ICar[]>('/cars');
+    return data;
+}
+
+export const createCar = async (carData: ICar): Promise<void> => {
+    await axiosInstance.post('/cars', carData);
 }
