8d0e095729ecfb935a122d81dda0ed9682f1b0b8
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-02-08 14:16:36 +0100

    працюю над доданням помилки

--- src/app/createCar/page.tsx
+++ src/app/createCar/page.tsx
@@ -1,72 +1,24 @@
-"use client"
-
-import React, {useState} from "react";
 import {createCar} from "@/server-actions/serverActions";
 import Menu from "@/components/menu/Menu";
-
-interface IErrors {
-    brand?: string;
-    price?: string;
-    year?: string;
-}
+import Form from "next/form";
 
 const CreateCarPage = () => {
-    const [formValues, setFormValues] = useState({brand: "", price: "", year: "",});
-
-    const [errors, setErrors] = useState<IErrors>({});
-
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-
-        const formData = new FormData(e.currentTarget);
-        const result = await createCar(formData);
-        setErrors(result || {});
-
-        if (!result) {
-            setFormValues({brand: "", price: "", year: ""});
-        }
-    };
-
-    const handleChange = ({target: {name, value}}: React.ChangeEvent<HTMLInputElement>) => {
-        setFormValues(prev => ({...prev, [name]: value}));
-    };
 
     return (
         <>
             <Menu/>
-            <form onSubmit={handleSubmit}>
+            <Form action={createCar}>
                 <div>
-                    <input
-                        type="text"
-                        name="brand"
-                        placeholder="Brand"
-                        value={formValues.brand}
-                        onChange={handleChange}
-                    />
-                    {errors.brand && <p>{errors.brand}</p>}
+                    <input type="text" name="brand" placeholder="Brand"/>
                 </div>
                 <div>
-                    <input
-                        type="number"
-                        name="price"
-                        placeholder="Price"
-                        value={formValues.price}
-                        onChange={handleChange}
-                    />
-                    {errors.price && <p>{errors.price}</p>}
+                    <input type="number" name="price" placeholder="Price"/>
                 </div>
                 <div>
-                    <input
-                        type="number"
-                        name="year"
-                        placeholder="Year"
-                        value={formValues.year}
-                        onChange={handleChange}
-                    />
-                    {errors.year && <p>{errors.year}</p>}
+                    <input type="number" name="year" placeholder="Year"/>
                 </div>
                 <button type="submit">Submit</button>
-            </form>
+            </Form>
         </>
     );
 };
