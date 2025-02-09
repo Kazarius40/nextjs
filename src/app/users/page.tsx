a159b748cb88e80665de2253460658125d42e3c4
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-02-09 06:42:27 +0100

    Лекція 3/3 ISR

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,14 +1,14 @@
 const UsersPage = async () => {
-    const users = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await fetch('https://jsonplaceholder.typicode.com/users', {next: {revalidate: 5}})
         .then(value => value.json());
 
 
     return (
         <div>
 
-            {users.map((user: { id: number, name: string }) => (<div key={user.id}>
-                {user.name}
-            </div>))}
+            <h2>{Date.now()}</h2>
+
+            {users.map((user: { id: number, name: string }) => (<div key={user.id}>{user.name}</div>))}
 
         </div>
     );
